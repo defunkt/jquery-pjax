8a48a53adbc6ebab7bfc64fcad04a8d5efc7f952
jejacks0n <jejacks0n@gmail.com>
2012-10-14 22:03:53 -0600

    Provides a strategy for providing additional data to pjax.

--- jquery.pjax.js
+++ jquery.pjax.js
@@ -238,7 +238,8 @@ function pjax(options) {
       title: container.title,
       container: context.selector,
       fragment: options.fragment,
-      timeout: options.timeout
+      timeout: options.timeout,
+      data: xhr.getResponseHeader('X-PJAX-DATA')
     }
 
     if (options.push || options.replace) {
