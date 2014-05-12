dee5ad8ccb212b7451515466ad3046c623be6797
Richard Kimber <richard@dogma.co.uk>
2014-05-12 07:54:45 +0100

    More unit tests for handleSubmit

--- test/unit/form.js
+++ test/unit/form.js
@@ -8,7 +8,7 @@ if ($.support.pjax) {
         window.iframeLoad = $.noop
         start()
       }
-      $("#qunit-fixture").append("<iframe src='form.html'>")
+      $("#qunit-fixture").append("<iframe src='basic_form.html'>")
     },
     teardown: function() {
       delete window.iframeLoad
@@ -25,5 +25,73 @@ if ($.support.pjax) {
       })
 
     frame.$("[name='submit']").click()
+  })
+
+  module("handleSubmit (multiple submit buttons)", {
+    setup: function() {
+      var self = this
+      stop()
+      window.iframeLoad = function(frame) {
+        self.frame = frame
+        window.iframeLoad = $.noop
+        start()
+      }
+      $("#qunit-fixture").append("<iframe src='form.html'>")
+    },
+    teardown: function() {
+      delete window.iframeLoad
+    }
+  })
+
+  asyncTest("Form POST records submitting element's value", function() {
+    var frame = this.frame
+
+    frame.$("#main")
+      .on("pjax:end", function() {
+        equal(frame.$("#submit").text(), "submit value")
+        start()
+      })
+
+    frame.$("input[name='submit']").click()
+  })
+
+  asyncTest("Form POST records submitting element's value, when input is a button", function() {
+    var frame = this.frame
+
+    frame.$("#main")
+      .on("pjax:end", function() {
+        equal(frame.$("#submit").text(), "submit value")
+        start()
+      })
+
+    frame.$("button[name='submit']").click()
+  })
+
+  module("handleSubmit (multiple forms)", {
+    setup: function() {
+      var self = this
+      stop()
+      window.iframeLoad = function(frame) {
+        self.frame = frame
+        window.iframeLoad = $.noop
+        start()
+      }
+      $("#qunit-fixture").append("<iframe src='multiple_forms.html'>")
+    },
+    teardown: function() {
+      delete window.iframeLoad
+    }
+  })
+
+  asyncTest("Form POST records submitting element's value", function() {
+    var frame = this.frame
+
+    frame.$("#main")
+      .on("pjax:end", function() {
+        equal(frame.$("#submit").text(), "submit value")
+        start()
+      })
+
+    frame.$("#form2 [name='submit']").click()
   })
 }
