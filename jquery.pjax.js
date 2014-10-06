863b802a84af8a9c72def4984d4d38163541fe6d
Mislav Marohnić <mislav.marohnic@gmail.com>
2014-10-06 13:02:46 +0200

    Fix pjax on iOS Simulator
    The iOS 8.0 simulator on Mavericks contains the following in its
    user-agent string: "iPhone; CPU iPhone OS 10_9_5 like Mac OS X"
    
    The "OS 1" bit gets interpreted by our regex as if it were iOS 1.0 and
    thus forcing pjax to be disabled since history manipulation APIs weren't
    stable until iOS 5.0.
    
    Fixes #443

--- jquery.pjax.js
+++ jquery.pjax.js
@@ -865,7 +865,7 @@ if ( $.inArray('state', $.event.props) < 0 )
 $.support.pjax =
   window.history && window.history.pushState && window.history.replaceState &&
   // pushState isn't reliable on iOS until 5.
-  !navigator.userAgent.match(/((iPod|iPhone|iPad).+\bOS\s+[1-4]|WebApps\/.+CFNetwork)/)
+  !navigator.userAgent.match(/((iPod|iPhone|iPad).+\bOS\s+[1-4]\D|WebApps\/.+CFNetwork)/)
 
 $.support.pjax ? enable() : disable()
 
