45a2e1e837c5332a9fe2ec93a9d008234d849e8d
Nolan Chou <zhounan0120@gmail.com>
2015-10-18 19:35:19 +0800

    Update jquery.pjax.js
    Fixed inline script need preload .js file.

--- jquery.pjax.js
+++ jquery.pjax.js
@@ -309,6 +309,10 @@ function pjax(options) {
       state: pjax.state,
       previousState: previousState
     })
+    
+    // include script file
+    executeScriptTags(container.scripts)
+    
     context.html(container.contents)
 
     // FF bug: Won't autofocus fields that are inserted via JS.
@@ -320,9 +324,7 @@ function pjax(options) {
     if (autofocusEl && document.activeElement !== autofocusEl) {
       autofocusEl.focus();
     }
-
-    executeScriptTags(container.scripts)
-
+    
     var scrollTo = options.scrollTo
 
     // Ensure browser scrolls to the element referenced by the URL anchor
